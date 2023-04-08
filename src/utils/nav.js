55847df432e42cd39bf345a465caff4e7b4db1a5
zZagro <samuel.nahm1@gmail.com>
2023-04-09 00:26:51 +0200

    optimization

--- src/utils/nav.js
+++ src/utils/nav.js
@@ -2,7 +2,7 @@ const nav = () => {
     const plugins = document.querySelector('.header__plugins');
     const dropdown = document.querySelector('.header__plugins-dropdown');
 
-    if (dropdown.style.opacity != '0')
+    if (dropdown.style.opacity != '0' && $(document).width() >= 768)
     {
         $(plugins).css('padding-bottom', 'var(--size-xxs)');
     }
