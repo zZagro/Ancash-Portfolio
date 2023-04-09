1fc704408e5f0ca0469232823b05b299673acfbf
zZagro <samuel.nahm1@gmail.com>
2023-04-09 05:03:09 +0200

    the shape positions and the number of shapes are not random anymore

--- src/utils/shapes.js
+++ src/utils/shapes.js
@@ -1,24 +1,11 @@
 const shapes = () => {
-    const shapes = document.querySelector('.shapes');
-    const addShapes = Math.floor(Math.random() * 3);
-    console.log(addShapes);
-    for (let j = 0; j < addShapes; j++)
-    {
-        $(shapes).append('<span class="shapes__circle"></span>');
-    }
-
     const plugins = document.querySelectorAll('.shapes__circle');
-    const colors = ['--clr-fuchsia', '--clr-cyan400', '--clr-cyan600', '--clr-cyan800'];
+    const colors = ['--clr-fuchsia', '--clr-cyan400'];
 
     for (let i = 0; i < plugins.length; i++) 
     {
         var item = colors[Math.floor(Math.random() * colors.length)];
         $(plugins[i]).css('border-color', 'var(' + item + ')');
-
-        var top = Math.floor(Math.random() * 100);
-        var left = Math.floor(Math.random() * 100);
-        $(plugins[i]).css('top', top + 'svh');
-        $(plugins[i]).css('left', left + 'svw');
     }
 };
 
