a74e0642ccb767b6c6ae560362f6976dfb732bbe
zZagro <samuel.nahm1@gmail.com>
2023-04-11 01:55:43 +0200

    forgot to add the new pages to vite to build lol

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,12 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         projects: resolve(__dirname, 'projects.html'),
+        contact: resolve(__dirname, 'contact.html'),
         projects_bz: resolve(__dirname, 'projects/bazaar.html'),
-        projectss_fc: resolve(__dirname, 'projects/fancy-crafting.html'),
+        projects_fc: resolve(__dirname, 'projects/fancy-crafting.html'),
+        projects_iecon: resolve(__dirname, 'projects/ieconomy.html'),
+        projects_ilib: resolve(__dirname, 'projects/ilibrary.html'),
+        projects_sockets: resolve(__dirname, 'projects/sockets.html'),
       },
     },
   },
